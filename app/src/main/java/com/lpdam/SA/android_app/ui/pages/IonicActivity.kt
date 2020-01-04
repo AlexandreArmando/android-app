bb72006757a7dfc7431d9b5fb887984a5274d226
Alex <alexandrearmando@hotmail.fr>
2020-01-05 00:15:23 +0100

    Add ionic activity

---
+++ app/src/main/java/com/lpdam/SA/android_app/ui/pages/IonicActivity.kt
@@ -0,0 +1,24 @@
+package com.lpdam.SA.android_app.ui.pages
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.lpdam.SA.android_app.R
+
+class IonicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ionic)
+
+
+
+    }
+
+}
