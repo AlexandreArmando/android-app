c10f33ddec8fb488211b5e3436e610ae80265c91
sabrihabib <sabri.habib@etu.unice.fr>
2020-01-06 01:50:05 +0100

    xamarinPage : content added to all the other activities and progression too

--- app/src/main/java/com/lpdam/SA/android_app/KotlinActivity.kt
+++ app/src/main/java/com/lpdam/SA/android_app/KotlinActivity.kt
@@ -1,19 +1,34 @@
 package com.lpdam.SA.android_app
 
+import android.annotation.TargetApi
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import androidx.annotation.RequiresApi
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_kotlin.*
+import kotlinx.android.synthetic.main.content_flutter.*
 
 class KotlinActivity : AppCompatActivity() {
 
+    @TargetApi(Build.VERSION_CODES.M)
+    @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin)
         setSupportActionBar(toolbar)
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            val url = Intent(Intent.ACTION_VIEW)
+            url.data = Uri.parse("https://kotlinlang.org/docs/tutorials/getting-started.html")
+            startActivity(url)
+        }
+
+        scrollView.setOnScrollChangeListener { view, scrollX, scrollY,  oldScrollX, oldScrollY ->
+            val percent : Float = scrollY.toFloat() / (linearLayout.height.toFloat() - 1536) * 100
+
+            MainActivity.progressionViewModel.kotlinProgression = "Kotlin ${percent.toInt()}%"
         }
     }
 }
